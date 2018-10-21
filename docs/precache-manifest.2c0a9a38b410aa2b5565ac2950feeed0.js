8778736448b8496e782b969a84969decc4a30762
Michal Raczkowski <michal.raczkowski.88@gmail.com>
2018-10-21 19:57:48 +0100

    Update

---
+++ docs/precache-manifest.2c0a9a38b410aa2b5565ac2950feeed0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3151eed1ec29e0997b18be08d41435e",
+    "url": "./static/media/home-background.e3151eed.jpg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "40fd48292bd71981328f",
+    "url": "./static/js/main.40fd4829.chunk.js"
+  },
+  {
+    "revision": "71ef590d20a1bfa439b2",
+    "url": "./static/js/1.71ef590d.chunk.js"
+  },
+  {
+    "revision": "40fd48292bd71981328f",
+    "url": "./static/css/main.6634f8b6.chunk.css"
+  },
+  {
+    "revision": "2cfffe78253d49f68bca31fc14796a4d",
+    "url": "./index.html"
+  }
+];
